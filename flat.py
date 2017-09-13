014a92ae98593a5c283d55e2f070278de4b0d6ad
brambilasantos <douglas13@astro.ufrj.br>
2017-09-13 17:35:43 -0300

    flat.py

--- flat.py
+++ flat.py
@@ -6,10 +6,8 @@ import numpy as np
 
 
 def abrindo_flat(caminho, quantidade, filtro):
-    
     flat_img = []
     flat_hdr= []
-    
     for i in range(quantidade):
         a=str(i+1)  #nao existe imagens 0, portanto é necessário incrementar o contador
         # decide a forma que os nomes estão escritos
@@ -20,15 +18,25 @@ def abrindo_flat(caminho, quantidade, filtro):
         #abre a imagem atual e o hedaer atual
         img, hdr = fits.getdata(flat_path, header=True)
         img = np.array(img, dtype='Float64')   
-
         flat_img.append(img)
         flat_hdr.append(hdr)
     return flat_img, flat_hdr
 
-def flat_bias():
+def correcao_flat_bias(caminho, quantidade, filtro):
     images_flat = abrindo_flat(caminho, quantidade, filtro)
-    flat_corrigido = []
+    bflat = []
     for i in range(len(images_flat[0])):
         correcao = images_flat[0][i] - bm(caminho,quantidade, filtro)
-        flat_corrigido.append(correcao)
-    return flat_corrigido
+        bflat.append(correcao)
+    return bflat
+
+def norm_flat(caminho, quantidade, filtro):
+    bflat = correcao_flat_bias(caminho, quantidade, filtro)
+    flat_norm = []
+    for i in range(len(bflat)):
+        flat_norm.append(bflat[i]/np.mean(bflat[i]))
+    return flat_norm
+
+def flat_field(caminho, quantidade, filtro):
+    median_flat = np.median(norm_flat(caminho, quantidade, filtro, axis=0)
+    return median_flat
